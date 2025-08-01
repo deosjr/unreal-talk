b4f9cede6fd97525fc7e1ffc5e6c77e529ef62c1
Sjoerd Dost <deosjr@gmail.com>
2025-08-01 17:08:57 +0200

    clean up whiskers script

--- scripts/6.scm
+++ scripts/6.scm
@@ -1,32 +1,18 @@
 ; This script provides basic support for whiskers (ie pointing at things)
-
 ; whisker length == page height
-(define (claim-has-whiskers p)
-  (hash-set! (datalog-idb (get-dl)) `(,this claims (,p has-whiskers #t)) #t)
-  (hash-set! (datalog-idb (get-dl)) `(,p has-whiskers #t) #t)
-  (Claim p 'has-whiskers #t))
-(define (claim-pointer-at p point)
-  (hash-set! (datalog-idb (get-dl)) `(,this claims (,p pointer-at ,point)) #t)
-  (hash-set! (datalog-idb (get-dl)) `(,p pointer-at ,point) #t)
-  (Claim p 'pointer-at point))
-(define (claim-point-at p q)
-  (hash-set! (datalog-idb (get-dl)) `(,this claims (,p points-at ,q)) #t)
-  (hash-set! (datalog-idb (get-dl)) `(,p points-at ,q) #t)
-  (Claim p 'points-at q))
 
 (When ((,?p wishes (,?p has-whiskers ,#t))) do
-  (claim-has-whiskers ?p))
+  (Claim-derived this ?p 'has-whiskers #t))
 
 ; page rotates around midpoint: from there to whisker end, add halfh + whisker length
 (When ((,?p has-whiskers #t)
        (,?p (page points) (,?ulhc ,?urhc ,?llhc ,?lrhc)))
- do (let* ((upvec (vec-from-to ?ulhc ?urhc))
-           (mid (vec-add ?ulhc (vec-mul upvec 0.5)))
-           (midx (inexact->exact (round (car mid)))) (midy (inexact->exact (round (cdr mid))))
-           (end (vec-add mid (vec-mul (vec-from-to ?lrhc ?urhc) 2)))
-           (endx (inexact->exact (round (car end)))) (endy (inexact->exact (round (cdr end)))))
+ do (let* ((mid (vec->ints (vec-add ?ulhc (vec-mul (vec-from-to ?ulhc ?urhc) 0.5))))
+           (midx (car mid)) (midy (cdr mid))
+           (end (vec->ints (vec-add mid (vec-mul (vec-from-to ?lrhc ?urhc) 2))))
+           (endx (car end)) (endy (cdr end)))
        (draw-line projection midx midy endx endy 0 255 0 2)
-       (claim-pointer-at ?p (cons endx endy))))
+       (Claim-derived this ?p 'pointer-at (cons endx endy))))
 
 ; NOTE: this fires for every page, since we can't calculate in the db atm!
 (When ((,?p pointer-at (,?px . ,?py))
@@ -34,4 +20,4 @@
   do (let* ((pts (points->bytevector ?ulhc ?urhc ?lrhc ?llhc))
             (ptr (bytevector->pointer pts))
             (test (point-polygon-test ptr 4 ?px ?py)))
-    (if (> test 0) (claim-point-at ?p ?q))))
+    (if (> test 0) (Claim-derived this ?p 'points-at ?q))))

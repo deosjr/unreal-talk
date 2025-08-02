1d4379d22a77194ad8ad205d3a227777a1f88fe0
Sjoerd Dost <deosjr@gmail.com>
2025-08-02 19:25:39 +0200

    draw words one-by-one in wiki

--- scripts/3.scm
+++ scripts/3.scm
@@ -19,21 +19,26 @@
   (string-join (map elem->str (cdr p)) ""))
 
 ; can assume axis-aligned region to draw in
-(define (draw-wiki-text str rotation ulhc urhc llhc lrhc)
+(define (draw-wiki-text sxml rotation ulhc urhc llhc lrhc)
   (let* ((font 0) (scale 0.5) (thickness 1) (padding 2)
-         (testsize (text-size str font scale thickness))
-         (width (car testsize))
-         (height (cadr testsize))
-         (baseline (caddr testsize)) ; todo: use baseline
-         (strptr (string->pointer str))
          (img (create-image 1280 720 16)) ; 16 is 3-channel CV8U
          (msk (create-image 1280 720 0)) ; 0 is 1-channel CV8U
          (center (vec->ints (vec-add ulhc (vec-mul (vec-from-to ulhc lrhc) 0.5))))
          (cx (car center)) (cy (cdr center))
-         (m (rotation-matrix-2d cx cy (- rotation) 1.0)) ; assumes counter-clockwise rotation!
-         (tx (+ (car ulhc) padding)) (ty (+ (cdr ulhc) height padding)))
-    (put-text img strptr tx ty font scale 255 255 255 thickness)  ; draw color to 3-channel img
-    (put-text msk strptr tx ty font scale 255 255 255 thickness)  ; draw white to 1-channel mask
+         (m (rotation-matrix-2d cx cy (- rotation) 1.0))) ; assumes counter-clockwise rotation!
+    (let loop ((lst sxml) (i 0) (tx (car ulhc)))
+      (if (and (< i 5) (not (null? lst)))
+          (let* ((str (elem->str (car lst)))
+                 (testsize (text-size str font scale thickness))
+                 (width (car testsize))
+                 (height (cadr testsize))
+                 (baseline (caddr testsize)) ; todo: use baseline
+                 (strptr (string->pointer str))
+                 (ntx (+ tx padding))
+                 (ty (+ (cdr ulhc) height padding)))
+            (put-text img strptr tx ty font scale 255 255 255 thickness)  ; draw color to 3-channel img
+            (put-text msk strptr tx ty font scale 255 255 255 thickness)  ; draw white to 1-channel mask
+            (loop (cdr lst) (+ i 1) (+ tx width padding)))))
     (warp-affine img img m 1280 720) ; officially doesn't support in-place modification?
     (warp-affine msk msk m 1280 720)
     (copy-from-to img projection msk)
@@ -47,7 +52,6 @@
       (if res
         (let* ((sxml (parse-ssax res))
                (matched ((sxpath '(// p)) sxml))
-               (first (car matched))
-               (str (p->str first)))
-          (draw-wiki-text str ?rotation ?ulhc ?urhc ?llhc ?lrhc))
+               (first (cdar matched)))
+          (draw-wiki-text first ?rotation ?ulhc ?urhc ?llhc ?lrhc))
         (Wish-derived this this 'labeled "LOADING"))))

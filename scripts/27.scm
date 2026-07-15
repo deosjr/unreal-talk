28f00aa849599edc66baeba9fff12c70e3ff6eec
Sjoerd Dost <deosjr@gmail.com>
2026-07-15 12:37:25 +0200

    REUS giant moving to indicator

--- scripts/27.scm
+++ scripts/27.scm
@@ -7,17 +7,18 @@
 
 (define pi 3.14159)
 
+(define (mod2pi x) (floor-remainder x (* 2 pi)))
+
 ; start giant on top of the world
 ; y-axis is flipped because img origin is top-left!!!
 (Remember 'giant-angle (* pi 0.5))
-(Remember 'angular-velocity 0.02)
+(Remember 'angular-velocity 0.0)
 
 (When ((time now ?t)
        (this giant-angle ?theta)
        (this angular-velocity ?omega))
  do (Wish this 'titled "REUS")
-    ; todo: modulo 2*pi
-    (let ((theta (+ ?theta ?omega)))
+    (let ((theta (mod2pi (+ ?theta ?omega))))
       (Remember 'giant-angle theta )))
 
 (When ((this (region tag) ?reg)
@@ -37,14 +38,32 @@
       ((and (< y 0) (< x 0)) (+ pi (- (asin y))))
       (else (+ (* 2 pi) (asin y))))))
 
+; try both ways around the circle
+; note angle is clockwise _decreasing_
+(define (diff-angles from to)
+  (let ((clockwise (if (> from to)
+          (- from to)  
+          (- (* 2 pi) (- to from))))
+        (counterclockwise (if (< from to)
+          (- to from)
+          (- (* 2 pi) (- from to)))))
+    (cons clockwise counterclockwise)))
+
 ; pointer intersects with world at angle
 ; if giant-angle is not that angle, then
 ; Remember new angle (but dont overshoot)
+; todo: remember the target, else it keeps going
+; when the target indicator is removed
 (When ((?someone wishes (giant moves-to ?p))
+       (this giant-angle ?a)
        (this world-center ?c))
- do (let ((target-angle (calc-angle ?c ?p)))
-      (Wish this 'subtitled (format #f "~a"
- target-angle))))
+ do (let* ((target (calc-angle ?c ?p))
+           (diffs (diff-angles ?a target))
+           (v 0.0))
+      (if (< (car diffs) (cdr diffs))
+        (set! v (max (- (car diffs)) -0.02))
+        (set! v (min (cdr diffs) 0.02)))
+      (Remember 'angular-velocity v)))
 
 (define (giant-coords c r theta)
   (let* ((gx (cos theta))
